60ce24dd45ed6a37e00be38869e70a2fff3eb64a
Felipe <felipe.jprodriguesc@gmail.com>
2025-01-23 17:45:36 -0300

    mudando o limite na tela

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
 }
 
 exibirMensagemInicial();
