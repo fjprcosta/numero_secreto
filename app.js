e8704645bb5b2abebc4c03308bb94da57ec32836
Felipe <felipe.jprodriguesc@gmail.com>
2025-01-23 18:01:39 -0300

    deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número de 1 a 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 100');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 50');
 }
 
 exibirMensagemInicial();
